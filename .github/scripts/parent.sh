413d5109dc83e212528c5d5ba00355407a82c953
ikoltsov <ilya@bridgerpay.com>
2022-05-04 21:19:38 +0300

    Fix

--- .github/scripts/parent.sh
+++ .github/scripts/parent.sh
@@ -1,9 +1,9 @@
-git log --oneline \                                                                      ✔  10193  20:50:23
+git log --oneline \
   | cut -f 1 -d' ' \
   | (while read commit ; do
        other_branches="$(git branch --contains $commit | egrep -v '^\* ')"
        if [ -n "${other_branches}" ] ; then
-         echo -e "${commit} is in:\n${other_branches}"
+         echo "${other_branches}"
          break
        fi
-     done)
+     done) | xargs
